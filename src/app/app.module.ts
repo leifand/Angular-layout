ad70186dd456d7b033d6f8291b6fd4bdaa0d1fcd
Leif Anderson <leif.v.anderson@gmail.com>
2017-09-18 19:25:08 -0500

    angular layout in progress ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { NavListComponent } from './nav/nav-list/nav-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent,
+    NavListComponent
   ],
   imports: [
     BrowserModule
